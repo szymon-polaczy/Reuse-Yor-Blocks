fb5555db880998a0dfbf8576e798c96bb90f7cfe
Szymon Polaczy <thomas_frey@tutanota.com>
2024-03-10 12:54:10 +0100

    Batman

---
+++ README.md
@@ -0,0 +1,2 @@
+# Requirements 
+Github CLI
---
+++ main.sh
@@ -0,0 +1,13 @@
+gh api \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  /orgs/osomstudio/repos --paginate > repos.json 
+
+cat repos.json | jq '.' > repos-pritified.json
+
+jq ".[].full_name" -r repos-pritified.json > repo_urls.txt
+
+cat repo_urls.txt | while read line || [[ -n $line ]];
+do
+	gh repo clone "$line"
+done
